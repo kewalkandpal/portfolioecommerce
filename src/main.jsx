6334aa32ca8bfedb5734e13e08bc62bbf004489c
kewalkandpal <kewalkv1@gmail.com>
2025-03-12 11:25:33 +0530

    initial co

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
@@ -8,11 +7,7 @@ import {store} from './redux/store';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <BrowserRouter basename="/portfolioecommerce">
-
         <App />
-      </BrowserRouter>
-
     </Provider>
   </StrictMode>,
 )
